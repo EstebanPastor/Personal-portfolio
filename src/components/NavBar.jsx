bee854637d7131bb46f8715361267c07a70ef40a
EstebanPastor <estebanandrespastor@gmail.com>
2025-03-09 04:20:50 -0300

    fix: :adhesive_bandage: Se fixeo un problema de estilos en el navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,6 @@ const NavBar = () => {
   return (
     <nav className="bg-black w-full z-50">
       <div className="flex justify-between items-center p-4">
-        <div className="text-white">Logo</div>
         <div className="md:hidden">
           <button onClick={() => setNav(!nav)} className="text-white">
             {nav ? "Cerrar" : "Menú"}
@@ -22,27 +21,11 @@ const NavBar = () => {
 
       {nav && (
         <div className="md:hidden">
-          <ul className="flex flex-col items-center bg-gradient-to-b from-black to-gray-800 text-gray-500 space-y-4 py-8">
-            <li>Inicio</li>
-            <li>Servicios</li>
-            <li>Contacto</li>
-
-            <li onClick={() => setLanguage(language === "es" ? "en" : "es")}>
-              {language === "es" ? "Inglés" : "Español"}
-            </li>
-          </ul>
+          <ul className="flex flex-col items-center bg-gradient-to-b from-black to-gray-800 text-gray-500 space-y-4 py-8"></ul>
         </div>
       )}
 
-      <div className="hidden md:flex justify-center space-x-8 text-white">
-        <div>Inicio</div>
-        <div>Servicios</div>
-        <div>Contacto</div>
-
-        <div onClick={() => setLanguage(language === "es" ? "en" : "es")}>
-          {language === "es" ? "Inglés" : "Español"}
-        </div>
-      </div>
+      <div className="hidden md:flex justify-center space-x-8 text-white"></div>
     </nav>
   );
 };
